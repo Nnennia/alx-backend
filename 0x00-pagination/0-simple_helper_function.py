8f245eb7dffe27a6de5e9328c372425170d30855
nnennia <106754370+Nnennia@users.noreply.github.com>
2023-05-24 07:45:54 +0100

    commit

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function that returns a tuple of size two
+containing a start index and an end index"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a tuple of size two containing a start index and an end index"""
+    end = int(page * page_size)
+    start = int(end - page_size)
+    return start , end
