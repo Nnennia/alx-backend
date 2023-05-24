1b0549cba7f2609f601fe679498fe6d61d092b08
Nnennia <nnenniaatuanya@gmail.com>
2023-05-24 08:02:37 +0100

    commit

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""""Pagination"""
+
+import csv
+import math
+from typing import LIst, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a tuple of size two containing a start index and an end index"""
+    end = int(page * page_size)
+    start = int(end - page_size)
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            """Get requested page"""
+            assert isinstance(page, int) and isinstance(page_size, int)
+            assert page > 0 and page_size > 0
+            try:
+                page_indexes = index_range(page, page_size)
+                start, stop = page_indexes
+                all_data = self.dataset()
+                return all_data[start: stop]
+            except Exception as e:
+                raise e
