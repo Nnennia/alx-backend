73fa0b61fdd2ac724223568445b25d34ff6647b4
Nnennia <nnenniaatuanya@gmail.com>
2023-05-28 10:17:31 +0100

    changes

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python3
-""""Function with the babel.localselector"""
+""" Task-1 Flask-Babel """
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
-
-
-class Config(object):
-    """"Configure class that handles the languages"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = "en"
-
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
+class Config:
+    """ Babel config
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ index function
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
 def get_locale():
-    """"Get locale function"""
+    """Get locale function"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, locale_selector=get_locale)
-
-
-@app.route("/")
-def index():
-    """render index.html"""
-    return render_template("2-index.html")
-
-
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run()
